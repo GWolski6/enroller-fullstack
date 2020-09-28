a5a73bd8014a3fbcb061e81a49186802e18b8cfa
Grzegorz Wolski <gwolski93@gmail.com>
2020-09-29 00:45:25 +0200

    lets start

--- src/main/java/com/company/enroller/App.java
+++ src/main/java/com/company/enroller/App.java
@@ -1,5 +1,6 @@
 package com.company.enroller;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
